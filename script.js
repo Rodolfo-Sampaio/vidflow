ca029d4d0039a4ec5777c985f02b47dc4803d7d4
Rodolfo-Sampaio <96917363+Rodolfo-Sampaio@users.noreply.github.com>
2024-01-10 18:16:56 -0300

    refactored with Async/Await

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch('http://localhost:3000/videos')
-	.then((res) => res.json())
-	.then((videos) =>
-		videos.forEach((video) => {
-			containerVideos.innerHTML += /*html*/ `
-         <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-               <img src="${video.imagem}" alt="Logo do Canal" class="img-canal">
-               <h3 class="titulo-video">${video.titulo}</h3>
-               <p class="titulo-canal">${video.descricao}</p>
-            </div>
-         </li>
-      `;
-		})
-	)
-	.catch((error) => {
-		containerVideos.innerHTML = /*html*/ `
-         <p>Houve um erro ao carregar os vídeos: ${error}</p>
-      `;
+async function buscarEMostrarVideos() {
+	const busca = await fetch('http://localhost:3000/videos');
+	const videos = await busca.json();
+
+	videos.forEach((video) => {
+		containerVideos.innerHTML += /*html*/ `
+            <li class="videos__item">
+               <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+               <div class="descricao-video">
+                  <img src="${video.imagem}" alt="Logo do Canal" class="img-canal">
+                  <h3 class="titulo-video">${video.titulo}</h3>
+                  <p class="titulo-canal">${video.descricao}</p>
+               </div>
+            </li>
+         `;
 	});
+}
+
+buscarEMostrarVideos();
